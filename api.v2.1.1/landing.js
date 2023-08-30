e578bc9a2aa1986d82394ba15b436e7ad55b156e
Daniel Archibald <daniel.danielarchibald@gmail.com>
2023-08-30 05:50:06 -0700

    -

--- api.v2.1.1/landing.js
+++ api.v2.1.1/landing.js
@@ -170,7 +170,7 @@ module.exports = {
   page += `
   <script>
   async function download(){
-    if (!window.confirm('Download as .CSV: YouTube Trending?')) return;
+    if (!window.confirm('Download as .CSV: youtube_trending?')) return;
     let csv = ${csv}
   var dataStr = "data:text/csv;charset=utf-8," + encodeURIComponent(csv);
   var anchor = document.getElementById('downloadAnchor');
